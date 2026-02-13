2e9558ea703a2158200c92524f1d28b2a5ffc55c
apoorvapendse <apoorvavpendse@gmail.com>
2026-02-14 00:16:49 +0530

    Open external links in new tab.
    Signed-off-by: apoorvapendse <apoorvavpendse@gmail.com>

--- src/message_row.ts
+++ src/message_row.ts
@@ -27,10 +27,27 @@ function render_avatar(avatar_url: string): HTMLElement {
     return div;
 }
 
+function preprocess_anchor_element(ele:HTMLAnchorElement){
+    const url = new URL(ele.getAttribute("href")!, window.location.href);
+    if (
+        url.hash === "" ||
+        url.href !== new URL(url.hash, window.location.href).href
+    ) {
+        ele.setAttribute("target", "_blank");
+        ele.setAttribute("rel", "noopener noreferrer");
+    }
+}
+
+function preprocess_message_content(html_content: string): DocumentFragment {
+    const template = document.createElement("template");
+    template.innerHTML = html_content;
+    template.content.querySelectorAll("a").forEach((ele) => preprocess_anchor_element(ele));
+    return template.content;
+}
+
 function render_message_content(content: string): HTMLElement {
     const div = document.createElement("div");
-    div.innerHTML = content;
-
+    div.append(preprocess_message_content(content));
     return div;
 }
 
