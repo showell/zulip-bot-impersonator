cecfaa1af4529166fe9d0118709a0d737ca842da
Steve Howell <showell30@yahoo.com>
2026-02-13 14:55:47 -0500

    Extract message_list.

--- src/message_pane.ts
+++ src/message_pane.ts
@@ -1,47 +1,7 @@
-import type { RawMessage, Topic } from "./model";
+import type { Topic } from "./model";
 
-import { MessageRow } from "./message_row";
+import { MessageList } from "./message_list";
 import { MessageViewHeader } from "./message_view_header";
-import * as model from "./model";
-
-class MessageList {
-    div: HTMLElement;
-    topic: Topic;
-
-    constructor(topic: Topic) {
-        const div = document.createElement("div");
-        div.style.maxHeight = "80vh";
-        div.style.overflowY = "auto";
-
-        this.topic = topic;
-        this.div = div;
-        this.populate();
-    }
-
-    populate() {
-        const div = this.div;
-        const topic = this.topic;
-
-        div.innerHTML = "";
-
-        const messages = model.messages_for_topic(topic);
-
-        let prev_sender_id: number | undefined;
-
-        for (const message of messages) {
-            let sender_id: number | undefined = message.sender_id;
-
-            if (sender_id === prev_sender_id) {
-                sender_id = undefined;
-            } else {
-                prev_sender_id = sender_id;
-            }
-
-            const row = new MessageRow(message, sender_id);
-            div.append(row.div);
-        }
-    }
-}
 
 export class MessagePane {
     div: HTMLElement;
