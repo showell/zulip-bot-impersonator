838bb603526a1ea4dc09ca365c2269380658c083
apoorvapendse <apoorvavpendse@gmail.com>
2026-02-16 00:03:32 +0530

    Add a ModalManager.

---
+++ src/modal_manager.ts
@@ -0,0 +1,21 @@
+class ModalManagerSingleton{
+    callbacks: ((e:PointerEvent) => void)[];
+    constructor() {
+        this.callbacks = [];
+        this.initialize();
+    }
+    register(callback:(e:PointerEvent)=>void) {
+        this.callbacks.push(callback)
+    }
+
+    private initialize() {
+        document.body.addEventListener("click", (e) => {
+            for (const cb of this.callbacks) {
+              cb(e)
+            }
+        })
+    }
+
+}
+
+export const ModalManager = new ModalManagerSingleton();
