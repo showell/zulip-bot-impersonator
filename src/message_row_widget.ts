0624c6adc0f9df7e51911bc12692c403d7210528
Steve Howell <showell30@yahoo.com>
2026-02-21 21:43:22 -0500

    Extract render_message_box.

--- src/message_row_widget.ts
+++ src/message_row_widget.ts
@@ -2,6 +2,20 @@ import type { MessageRow } from "./row_types";
 
 import { render_message_content } from "./message_content";
 
+function render_message_box() {
+    const div = document.createElement("div");
+
+    div.style.paddingTop = "5px";
+    div.style.marginBottom = "5px";
+    div.style.borderBottom = "1px dotted #000080";
+    div.style.maxWidth = "500px";
+    div.style.fontSize = "16px";
+    div.style.fontFamily = `"Source Sans 3 VF", sans-serif`;
+    div.style.lineHeight = "22.4px";
+
+    return div;
+}
+
 function render_sender_name(sender_name: string): HTMLElement {
     const div = document.createElement("div");
     div.innerText = sender_name;
@@ -30,16 +44,7 @@ export class MessageRowWidget {
     div: HTMLElement;
 
     constructor(message_row: MessageRow, show_sender: boolean) {
-        const div = document.createElement("div");
-
-        div.style.paddingTop = "5px";
-        div.style.marginBottom = "5px";
-        div.style.borderBottom = "1px dotted #000080";
-        div.style.maxWidth = "500px";
-        div.style.fontSize = "16px";
-        div.style.fontFamily = `"Source Sans 3 VF", sans-serif`;
-        div.style.color = "rgb(38, 38, 38)";
-        div.style.lineHeight = "22.4px";
+        const div = render_message_box();
 
         if (message_row.unread()) {
             div.style.backgroundColor = "lavender";
