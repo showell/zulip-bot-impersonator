7fb9abd1b1062cb58531ac43665722680333aea0
Steve Howell <showell30@yahoo.com>
2026-02-23 23:19:41 -0500

    Add image-fetching code (very messy).

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,11 @@
 import * as database from "./backend/database";
 import * as model from "./backend/model";
+import * as zulip_client from "./backend/zulip_client";
+
+async function test_image() {
+    const url = "https://macandcheese.zulipchat.com/api/v1/user_uploads/2943/ABnDA3NW8ggkafWrk_hDD-HM/image.png"
+    await zulip_client.fetch_image(url);
+}
 
 async function test(): Promise<void> {
     await database.fetch_original_data();
@@ -7,4 +13,4 @@ async function test(): Promise<void> {
     console.log(model.get_channel_rows().slice(0, 2));
 }
 
-test();
+test_image();
