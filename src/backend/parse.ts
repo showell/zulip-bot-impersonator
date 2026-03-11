44d3d4db401896ef6e1e16eb80a6eb8a79bc3600
Steve Howell <showell30@yahoo.com>
2026-03-11 12:55:17 -0400

    Suppress parser when using node.

--- src/backend/parse.ts
+++ src/backend/parse.ts
@@ -1,7 +1,11 @@
 import { get_dom_parser } from "../parser";
 import type { Message } from "./db_types";
 
-export function parse_content(message: Message) {
+export function parse_content(message: Message): void {
+    if (typeof window === "undefined") {
+        return;
+    }
+
     const parser = get_dom_parser();
     const doc = parser.parseFromString(message.content, "text/html");
 
