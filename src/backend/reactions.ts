92ec7c6846b1e2cb58562934c515cc9bce983e59
Steve Howell <showell30@yahoo.com>
2026-03-12 20:47:39 -0400

    Fix add/delete edge cases from refactoring.

--- src/backend/reactions.ts
+++ src/backend/reactions.ts
@@ -43,37 +43,54 @@ export class ReactionsMap {
 
     process_add_event(event: ReactionEvent) {
         const message_id = event.message_id;
-        const reactions = this.message_reactions_map.get(message_id);
+        const reactions = this.message_reactions_map.get(message_id) ?? [];
 
-        if (reactions) {
-            const reaction = reactions.find(
-                (reaction) => reaction.emoji_code === event.emoji_code,
-            );
-            if (reaction) {
-                reaction.user_ids.add(event.user_id);
-            } else {
-                reactions.push({
-                    message_id: event.message_id,
-                    emoji_code: event.emoji_code,
-                    emoji_name: event.emoji_name,
-                    user_ids: new Set<number>([event.user_id]),
-                });
-            }
+        const reaction = reactions.find(
+            (reaction) => reaction.emoji_code === event.emoji_code,
+        );
+
+        if (reaction) {
+            reaction.user_ids.add(event.user_id);
+        } else {
+            reactions.push({
+                message_id: event.message_id,
+                emoji_code: event.emoji_code,
+                emoji_name: event.emoji_name,
+                user_ids: new Set<number>([event.user_id]),
+            });
         }
+
+        this.message_reactions_map.set(message_id, reactions);
     }
 
     process_remove_event(event: ReactionEvent) {
         const message_id = event.message_id;
         const reactions = this.message_reactions_map.get(message_id);
 
-        if (reactions) {
-            const reaction_idx = reactions.findIndex(
-                (reaction) => reaction.emoji_code === event.emoji_code,
-            );
-            const reaction = reactions[reaction_idx];
-            reaction?.user_ids.delete(event.user_id);
-            if (reaction?.user_ids.size === 0) {
-                reactions.splice(reaction_idx, 1);
+        if (!reactions) {
+            console.warn("tried to remove non-existent reaction");
+            return;
+        }
+
+        const reaction_idx = reactions.findIndex(
+            (reaction) => reaction.emoji_code === event.emoji_code,
+        );
+
+        const reaction = reactions[reaction_idx];
+
+        if (!reaction) {
+            console.warn("tried to remove non-existent reaction");
+            return;
+        }
+
+        reaction.user_ids.delete(event.user_id);
+
+        if (reaction.user_ids.size === 0) {
+            reactions.splice(reaction_idx, 1);
+
+            if (reactions.length === 0) {
+                this.message_reactions_map.delete(message_id);
+                console.log("removed message_id from reactions map");
             }
         }
     }
